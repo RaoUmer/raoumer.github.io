0c76888a35736ae934d08853f70141cba3dbec5a
Rao Muhammad Umer <engr.raoumer943@gmail.com>
2016-05-24 23:37:28 +0500

    Delete application.js

--- javascripts/application.js
+++
@@ -1,9 +0,0 @@
-var meny = Meny.create({
-  menuElement: document.querySelector( '.meny' ),
-  contentsElement: document.querySelector( '.contents' ),
-  position: 'left',
-  width: 260,
-  threshold: 40,
-  mouse: true,
-  touch: true
-});
