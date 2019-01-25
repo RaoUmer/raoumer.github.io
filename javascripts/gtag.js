df1eb81945b7bcf2df02207d07ba0a37a6885360
Rao Muhammad Umer <engr.raoumer943@gmail.com>
2019-01-25 21:03:08 +0100

    Create gtag.js

---
+++ javascripts/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-77283979-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-77283979-1', { 'optimize_id': 'GTM-TKHS74Z'});
+</script>
